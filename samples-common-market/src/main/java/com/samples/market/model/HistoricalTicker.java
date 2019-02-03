a283e30efd0fe4d6d57ec33a8449f965868a6209
chiusday <chiusday@hotmail.sg>
2019-02-03 12:49:34 +0800

    Initial commit

---
+++ samples-common-market/src/main/java/com/samples/market/model/HistoricalTicker.java
@@ -0,0 +1,13 @@
+package com.samples.market.model;
+
+public class HistoricalTicker extends Ticker {
+	public String priceDate;
+
+	public String getPriceDate() {
+		return priceDate;
+	}
+
+	public void setPriceDate(String priceDate) {
+		this.priceDate = priceDate;
+	}
+}
---
+++ samples-common-market/src/main/java/com/samples/market/model/TickerBase.java
@@ -0,0 +1,14 @@
+package com.samples.market.model;
+
+public class TickerBase {
+	protected String symbol;
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+}
